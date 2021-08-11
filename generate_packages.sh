bdb028fa4fc9d00b688883dcf064bfdf3fbcb6bc
mlbiam <marc@tremolo.io>
2021-08-11 10:32:45 -0400

    updates for ou2

--- generate_packages.sh
+++ generate_packages.sh
@@ -8,8 +8,13 @@ for d in openunison-*/ ; do
     helm package $d
 done
 
+for d in orchestra*/ ; do
+    echo "$d"
+    helm package $d
+done
+
 mv *.tgz /tmp/helm
 
-helm repo index /tmp/helm --url https://nexus.tremolo.io/repository/helm
+helm repo index /tmp/helm --url https://nexus.tremolo.io/repository/helm-betas
 
-aws s3 sync /tmp/helm/ s3://tremolosecurity-maven/repository/helm/
+aws s3 sync /tmp/helm/ s3://tremolosecurity-maven/repository/helm-betas/
