658123756de2b2cae962466bcec91b78fe29cf51
mlbiam <marc@tremolo.io>
2022-03-18 19:37:29 -0400

    index all charts

--- generate_packages.sh
+++ generate_packages.sh
@@ -13,8 +13,10 @@ for d in orchestra*/ ; do
     helm package $d
 done
 
+aws s3 sync s3://tremolosecurity-maven/repository/helm-betas/ /tmp/helm/ 
+
 mv *.tgz /tmp/helm
 
-helm repo index /tmp/helm --url https://nexus.tremolo.io/repository/helm
+helm repo index /tmp/helm --url https://nexus.tremolo.io/repository/helm-betas
 
-aws s3 sync /tmp/helm/ s3://tremolosecurity-maven/repository/helm/
+aws s3 sync /tmp/helm/ s3://tremolosecurity-maven/repository/helm-betas/
